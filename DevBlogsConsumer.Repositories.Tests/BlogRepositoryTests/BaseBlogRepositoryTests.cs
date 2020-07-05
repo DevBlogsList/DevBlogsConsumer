8eba11260915daa49096dca7a80d2574d8f52837
Steven McLintock <stevenmclintock@fastmail.com>
2020-07-04 20:07:09 -0400

    Amend base tests for blog repo

--- DevBlogsConsumer.Repositories.Tests/BlogRepositoryTests/BaseBlogRepositoryTests.cs
+++ DevBlogsConsumer.Repositories.Tests/BlogRepositoryTests/BaseBlogRepositoryTests.cs
@@ -7,9 +7,17 @@ namespace DevBlogsConsumer.Repositories.Tests.BlogRepositoryTests
 {
     public abstract class BaseBlogRepositoryTests
     {
+        #region Constants
+
         public const string TEST_CATEGORY = "Blog Repository Tests";
+
+        #endregion
+
+        #region Properties
 
         public Mock<IMongoCollection<Blog>> _mockBlogCollection;
         public IBlogRepository _blogRepository;
+
+        #endregion
     }
 }
