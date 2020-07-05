753d4b55012971af7e6ba3785ec80a380a018064
Steven McLintock <stevenmclintock@fastmail.com>
2020-07-05 18:49:42 -0400

    Article service

---
+++ DevBlogsConsumer.Services.Tests/ArticleServiceTests/InsertArticlesForBlogTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevBlogsConsumer.Services.Tests.ArticleServiceTests
+{
+    [TestCategory(TEST_CATEGORY)]
+    [TestClass]
+    public class InsertArticlesForBlogTests : BaseArticleServiceTests
+    {
+        [TestMethod]
+        public void InsertArticlesForBlog_DoesNotThrowException()
+        {
+            // TODO: Insert unit test functionality
+        }
+
+        [TestMethod]
+        public void InsertArticlesForBlog_WillRemoveDuplicates()
+        {
+            // TODO: Insert unit test functionality
+        }
+
+        [TestMethod]
+        public void InsertArticlesForBlog_WillSkipInsertIfNoArticles()
+        {
+            // TODO: Insert unit test functionality
+        }
+
+        [TestMethod]
+        public void InsertArticlesForBlog_DoesNowAllowNullBlogId()
+        {
+            // TODO: Insert unit test functionality
+        }
+
+        [TestMethod]
+        public void InsertArticlesForBlog_DoesNowAllowEmptyBlogId()
+        {
+            // TODO: Insert unit test functionality
+        }
+
+        [TestMethod]
+        public void InsertArticlesForBlog_DoesNowAllowNullArticles()
+        {
+            // TODO: Insert unit test functionality
+        }
+
+        [TestMethod]
+        public void InsertArticlesForBlog_DoesNowAllowEmptyArticles()
+        {
+            // TODO: Insert unit test functionality
+        }
+    }
+}
---
+++ DevBlogsConsumer.Services/ArticleService.cs
@@ -0,0 +1,37 @@
+﻿using Dawn;
+using DevBlogsConsumer.Repositories.Contracts;
+using DevBlogsConsumer.Repositories.Interfaces;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevBlogsConsumer.Services
+{
+    public class ArticleService
+    {
+        private IArticleRepository _articleRepository;
+
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public void InsertArticlesForBlog(string blogId, IEnumerable<Article> articles)
+        {
+            Guard.Argument(blogId, nameof(blogId)).NotNull().NotEmpty();
+            Guard.Argument(blogId, nameof(articles)).NotNull().NotEmpty();
+
+            // Retrieve the articles already in the database for the specified blog
+            IEnumerable<string> articlesForBlogInDatabase =
+                _articleRepository.GetArticleIdsForBlog(blogId).Result.ToList();
+
+            // Remove any duplicate entries
+            if (articlesForBlogInDatabase.Any())
+                articles = articles.Where(x => !articlesForBlogInDatabase.Contains(x.ArticleId));
+
+            // Insert the new articles into the database
+            if (articles.Any())
+                _articleRepository.InsertArticles(articles);
+        }
+    }
+}
